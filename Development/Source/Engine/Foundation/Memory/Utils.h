6285e5b90565eedb35c340281ad0fd1fe32f0884
Oleksandr Novytskyi <ddelion.ua@gmail.com>
2021-04-29 21:05:50 +0300

    Added helper functions for memory sizes.

--- Development/Source/Engine/Foundation/Memory/Utils.h
+++ Development/Source/Engine/Foundation/Memory/Utils.h
@@ -4,6 +4,16 @@ namespace hd
 {
     namespace mem
     {
+        inline constexpr size_t KB(size_t value) { return value * 1024U; }
+        inline constexpr size_t MB(size_t value) { return KB(value) * 1024U; }
+        inline constexpr size_t GB(size_t value) { return MB(value) * 1024U; }
+        inline constexpr size_t TB(size_t value) { return GB(value) * 1024U; }
+
+        inline constexpr size_t ToKB(size_t value) { return value / 1024U; }
+        inline constexpr size_t ToMB(size_t value) { return ToKB(value) / 1024U; }
+        inline constexpr size_t ToGB(size_t value) { return ToMB(value) / 1024U; }
+        inline constexpr size_t ToTB(size_t value) { return ToGB(value) / 1024U; }
+
         size_t AlignBelow(size_t value, size_t alignment);
         void* AlignBelow(void* pointer, size_t alignment);
 
