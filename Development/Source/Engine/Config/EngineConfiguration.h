3923a1fffe2e9d2d8073be17786369052fc865e6
Oleksandr Novytskyi <ddelion.ua@gmail.com>
2021-05-15 03:07:31 +0300

    DX12 specific engine configuration.

--- Development/Source/Engine/Config/EngineConfiguration.h
+++ Development/Source/Engine/Config/EngineConfiguration.h
@@ -7,5 +7,13 @@ namespace hd
         inline constexpr char8_t const* LogFilepath() { return u8"headless_droid_log.txt"; }
 
         inline constexpr uint32_t MaxFrameLatency() { return 3; }
+
+#if defined(HD_GRAPHICS_API_DX12)
+        inline constexpr size_t MaxTransitionRequests() { return 128; }
+        inline constexpr uint32_t MaxDescriptorsRTV() { return 1000000; }
+        inline constexpr uint32_t MaxDescriptorsDSV() { return 1000000; }
+        inline constexpr uint32_t MaxDescriptorsSRV() { return 1000000; }
+        inline constexpr uint32_t MaxDescriptorsSampler() { return 2048; }
+#endif
     }
 }
